2434ccdc556769a5e298935a0bff27a2a2139663
Eva Pace <eba.pachi@gmail.com>
2024-12-15 15:20:50 -0300

    step31: get cursor position pt 1

--- main.c
+++ main.c
@@ -66,13 +66,30 @@ char editor_read_key(void) {
     return c;
 }
 
+int get_cursor_position(int *rows, int *cols) {
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    printf("\r\n");
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1) {
+        if (iscntrl(c)) {
+            printf("%d\r\n", c);
+        } else {
+            printf("%d ('%c')\r\n", c, c);
+        }
+    }
+
+    editor_read_key();
+    return -1;
+}
+
 int get_window_size(int *rows, int *cols) {
     struct winsize ws;
 
     if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
         if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
-        editor_read_key();
-        return -1;
+        return get_cursor_position(rows, cols);
     }
 
     *cols = ws.ws_col;
