9466feb3bf60efaacb3d58090e08bea4a24635a1
Eva Pace <eba.pachi@gmail.com>
2025-01-25 20:30:57 -0300

    step78: move left goes up

--- main.c
+++ main.c
@@ -302,8 +302,12 @@ void editor_move_cursor(int key) {
 
     switch (key) {
         case ARROW_LEFT:
-            if (E.cx != 0)
+            if (E.cx != 0) {
                 E.cx--;
+            } else if (E.cy > 0) {
+                E.cy--;
+                E.cx = E.row[E.cy].size;
+            }
             break;
         case ARROW_DOWN:
             if (E.cy < E.numrows)
