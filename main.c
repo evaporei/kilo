c435b74e10570e1c2e5f4408623ac8dc8c440374
Eva Pace <eba.pachi@gmail.com>
2025-01-24 21:18:02 -0300

    step50: detect page up & down

--- main.c
+++ main.c
@@ -20,6 +20,8 @@ enum EditorKey {
     ARROW_RIGHT,
     ARROW_UP,
     ARROW_DOWN,
+    PAGE_UP,
+    PAGE_DOWN,
 };
 
 /*** data ***/
@@ -80,6 +82,16 @@ int editor_read_key(void) {
         if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
         if (seq[0] == '[') {
+            if (seq[1] >= '0' && seq[1] <= '9') {
+                if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+                if (seq[2] == '~') {
+                    switch (seq[1]) {
+                        case '5': return PAGE_UP;
+                        case '6': return PAGE_DOWN;
+                    }
+                }
+            }
+
             switch (seq[1]) {
                 case 'A': return ARROW_UP;
                 case 'B': return ARROW_DOWN;
