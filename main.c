cb38478cb64919793ec690f41598945154427cb5
Eva Pace <eba.pachi@gmail.com>
2025-01-26 14:17:05 -0300

    step91: make End key relative to file instead of screen

--- main.c
+++ main.c
@@ -392,7 +392,8 @@ void editor_process_keypress(void) {
             E.cx = 0;
             break;
         case END_KEY:
-            E.cx = E.screencols - 1;
+            if (E.cy < E.numrows)
+                E.cx = E.row[E.cy].size;
             break;
         case PAGE_UP:
         case PAGE_DOWN:
