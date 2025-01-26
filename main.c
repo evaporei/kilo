32733353790830ec19ae74dc678f1654bf4f1fa5
Eva Pace <eba.pachi@gmail.com>
2025-01-26 14:09:38 -0300

    step89: set rx in scroll fn

--- main.c
+++ main.c
@@ -266,7 +266,9 @@ void abuf_free(struct AppendBuf *abuf) {
 /*** output ***/
 
 void editor_scroll(void) {
-    E.rx = E.cx;
+    E.rx = 0;
+    if (E.cy < E.numrows)
+        E.rx = editor_row_cx_to_rx(&E.row[E.cy], E.cx);
 
     if (E.cy < E.rowoff)
         E.rowoff = E.cy;
