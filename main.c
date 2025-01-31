fb5eb310827d6ff151880c790002c04a01ff1466
Eva Pace <eba.pachi@gmail.com>
2025-01-31 14:47:51 -0300

    step161: consider syntax in update

--- main.c
+++ main.c
@@ -224,6 +224,8 @@ void editor_update_syntax(Row *row) {
     row->hl = realloc(row->hl, row->rsize);
     memset(row->hl, HL_NORMAL, row->rsize);
 
+    if (E.syntax == NULL) return;
+
     int prev_sep = 1;
 
     int i = 0;
@@ -231,12 +233,14 @@ void editor_update_syntax(Row *row) {
         char c = row->render[i];
         unsigned char prev_hl = (i > 0) ? row->hl[i - 1] : HL_NORMAL;
 
-        if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER) ||
-            (c == '.' && prev_hl == HL_NUMBER)) {
-            row->hl[i] = HL_NUMBER;
-            i++;
-            prev_sep = 0;
-            continue;
+        if (E.syntax->flags & HL_HIGHLIGHT_NUMBERS) {
+            if (isdigit(c) && (prev_sep || prev_hl == HL_NUMBER) ||
+                    (c == '.' && prev_hl == HL_NUMBER)) {
+                row->hl[i] = HL_NUMBER;
+                i++;
+                prev_sep = 0;
+                continue;
+            }
         }
 
         prev_sep = is_separator(c);
