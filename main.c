a1c8395898ad138101f9be1812fe21666f83c1bc
Eva Pace <eba.pachi@gmail.com>
2025-01-31 15:29:34 -0300

    step165: string variant to hl enum

--- main.c
+++ main.c
@@ -40,6 +40,7 @@ enum EditorKey {
 
 enum EditorHighlight {
     HL_NORMAL = 0,
+    HL_STRING,
     HL_NUMBER,
     HL_MATCH
 };
@@ -250,6 +251,7 @@ void editor_update_syntax(Row *row) {
 
 int editor_syntax_to_color(int hl) {
     switch (hl) {
+        case HL_STRING: return 35;
         case HL_NUMBER: return 31;
         case HL_MATCH: return 34;
         default: return 37;
