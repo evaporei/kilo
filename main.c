399eaa97bc081300aefa303273101e51a506404d
Eva Pace <eba.pachi@gmail.com>
2025-01-30 23:22:21 -0300

    step145: update syntax fn

--- main.c
+++ main.c
@@ -190,6 +190,19 @@ int get_window_size(int *rows, int *cols) {
     return 0;
 }
 
+/*** syntax highlighting ***/
+
+void editor_update_syntax(Row *row) {
+    row->hl = realloc(row->hl, row->rsize);
+    memset(row->hl, HL_NORMAL, row->rsize);
+
+    for (int i = 0; i < row->rsize; i++) {
+        if (isdigit(row->render[i])) {
+            row->hl[i] = HL_NUMBER;
+        }
+    }
+}
+
 /*** row operations ***/
 
 int editor_row_cx_to_rx(Row *row, int cx) {
